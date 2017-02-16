ea513de05e2da022ca9488946cf620f00df44189
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-16 23:41:50 +0000

    Modified application/user mailer with default email and subject line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def order_confirmation(order)
+    @order = order
+    @url  = 'http://example.com/login'
+    mail(to: @order.email, subject: 'Your Jungle order has been placed. Order id: #{@order.id}')
+  end
+
 end
