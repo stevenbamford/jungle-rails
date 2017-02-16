0ffa46c6b52c8d8958cfe98893f2f14204118047
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-16 20:05:11 +0000

    Added error messages to registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   def new
-    end
+    @user = User.new
+  end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/register'
+      render :new
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+
+  validates :email, uniqueness: true
   has_secure_password
 end
