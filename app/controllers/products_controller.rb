edd9219eb06e82e4b0bfce69b650d3fc00fcc0af
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-18 22:59:46 +0000

    calculated avg_rating in products_controller and stored in @avg_rating

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,11 @@ class ProductsController < ApplicationController
     @product = Product.find params[:id]
     @reviews = Review.where(product_id: @product.id).sort { |a,b| b.created_at <=> a.created_at }
     @new_review = Review.new(product: @product)
+    @ratings = 0
+    @reviews.each do |review|
+      @ratings += review.rating
+    end
+    @avg_rating = @ratings / @reviews.length
   end
 
 end
