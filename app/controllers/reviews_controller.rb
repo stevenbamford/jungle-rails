c98b347c361d790c5bc30ce67e17008947d9bca3
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-18 01:13:19 +0000

    Reviews successfully added through form, and redirected back to product page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,22 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find params[:product_id]
+    @new_review = @product.reviews.new(review_params)
+    @new_review.user_id = current_user.id
+
+    if @new_review.save
+      redirect_to [@product], notice: 'Review created!'
+    else
+      redirect_to [@product],  notice: 'There was a problem adding your review.'
+    end
   end
+
+
+  private
+    def review_params
+      params.require(:review).permit(
+          :description,
+          :rating,
+        )
+    end
 end
