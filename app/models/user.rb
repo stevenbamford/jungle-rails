9fa015996e01e3a1d9879fe6ccd005c8c34bdbf1
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-20 20:48:42 +0000

    Added minimum password length of 8 to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: {case_sensitive: false}, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
 end
