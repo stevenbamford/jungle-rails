dd70853b99b214b07eb2faaee51304742e78ff49
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-20 19:55:49 +0000

    All validations for creating product pass tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,12 +8,29 @@ RSpec.describe Product, type: :model do
       expect(@product.save).to be true
     end
 
-    it 'should not save a product if name is not set' do
+    it 'should show correct error message if name is not set' do
       @category = Category.new(name: "Best Category")
-      @product = Product.new(name: nil, price: 500, quantity: 30, category: @category)
-      expect(@product.save).to be false
+      @product = Product.create(name: nil, price: 500, quantity: 30, category: @category)
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
+    it 'should show correct error message if price is not set' do
+      @category = Category.new(name: "Best Category")
+      @product = Product.create(name: "Best Product", price: nil, quantity: 30, category: @category)
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'should show correct error message if quantity is not set' do
+      @category = Category.new(name: "Best Category")
+      @product = Product.create(name: "Best Product", price: 500, quantity: nil, category: @category)
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should show correct error message if category is not set' do
+      @category = Category.new(name: "Best Category")
+      @product = Product.create(name: "Best Product", price: 500, quantity: 30, category: nil)
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
 
   end
 end
