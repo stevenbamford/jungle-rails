1d37a962b76fcb41aea0e265d08ada02c168fbe1
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-20 22:32:19 +0000

    Added authenticate class method to User model & invoked in sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ActiveRecord::Base
   has_secure_password
 
 
-  def authenticate_with_credentials(email, password)
-
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
   end
 
 end
