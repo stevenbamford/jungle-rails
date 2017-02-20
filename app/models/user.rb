d0eebe678a2d8d4eac5915268912c281638fa5f3
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-20 20:40:17 +0000

    Added case insensitivity to email validation, and validated presence of all fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-
-  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: {case_sensitive: false}, presence: true
+  validates :password, presence: true
   has_secure_password
 end
