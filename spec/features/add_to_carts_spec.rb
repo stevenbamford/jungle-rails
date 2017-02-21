4a8e690f494eb1cbd4792d09ea4e67f9e807f28a
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-21 20:55:22 +0000

    Test feature: click add to cart updates count of items in cart

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+)
+    end
+  end
+
+  scenario "Click on product" do
+
+    visit root_path
+
+    first('article.product').find_link("Add").click
+
+    sleep(3)
+
+    page.has_content?("My Cart (1)")
+    save_screenshot
+
+  end
+
+end
